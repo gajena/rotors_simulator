128c0774a6fd446f77f15aeae3123dd0a5584670
gn <gajendranagar02@gmail.com>
2021-12-28 16:58:25 +0530

    Single slungload working with single quadrotor

--- rotors_gazebo_plugins/include/rotors_gazebo_plugins/gazebo_cable_plugin.h
+++ rotors_gazebo_plugins/include/rotors_gazebo_plugins/gazebo_cable_plugin.h
@@ -3,39 +3,29 @@
 #include <stdio.h>
 
 #include <boost/bind.hpp>
-#include <cmath>
 #include <gazebo/common/Plugin.hh>
 #include <gazebo/common/common.hh>
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
-#include <random>
 
 #include "rotors_gazebo_plugins/common.h"
 #include "rotors_gazebo_plugins/sdf_api_wrapper.hpp"
 
 namespace gazebo {
 
-static const std::string kDefaultParentFrameId = "world";
-static const std::string kDefaultChildFrameId = "odometry_sensor";
-static const std::string kDefaultLinkName = "odometry_sensor_link";
-static const std::string kDefaultJointName = "odometry_sensor_joint";
 static const std::string kDefaultPosePubTopic = "ball/pose";
 
 class GazeboCablePlugin : public ModelPlugin {
  public:
   GazeboCablePlugin()
       : ModelPlugin(),
-        parent_frame_id_(kDefaultParentFrameId),
-        child_frame_id_(kDefaultChildFrameId),
-        link_name_(kDefaultLinkName),
-        joint_name_(kDefaultJointName),
         pose_pub_topic_(kDefaultPosePubTopic),
         pubs_and_subs_created_(false),
-        ball_radius_(0.0),
-        ball_weight_(0.0),
+        ball_mass_(0.0),
+        mav_mass_(0.0),
         cable_length_(0.0) {}
 
-  ~GazeboCablePlugin();
+  GazeboCablePlugin::~GazeboCablePlugin() {}
 
   void InitializeParams();
   void Publish();
@@ -49,31 +39,22 @@ class GazeboCablePlugin : public ModelPlugin {
   void CreatePubsAndSubs();
 
   std::string namespace_;
-  std::string parent_frame_id_;
-  std::string child_frame_id_;
-  std::string link_name_;
-  std::string joint_name_;
-
   std::string pose_pub_topic_;
 
-  double ball_radius_;
-  double ball_weight_;
+  double mav_mass_;
+  double ball_mass_;
   double cable_length_;
 
   gazebo::transport::NodePtr node_handle_;
 
   gazebo::transport::PublisherPtr pose_pub_;
-  gazebo::transport::PublisherPtr broadcast_transform_pub_;
 
   physics::WorldPtr world_;
   physics::ModelPtr model_;
   physics::LinkPtr link_;
-  physics::JointPtr joint_;
-  physics::EntityPtr parent_link_;
+  physics::LinkPtr parent_link_;
 
   event::ConnectionPtr updateConnection_;
-  boost::thread callback_queue_thread_;
-  void QueueThread();
 };
 
 };  // namespace gazebo
--- rotors_gazebo_plugins/src/gazebo_cable_plugin.cpp
+++ rotors_gazebo_plugins/src/gazebo_cable_plugin.cpp
@@ -1,74 +1,52 @@
 #include "rotors_gazebo_plugins/gazebo_cable_plugin.h"
-
 #include <rotors_gazebo_plugins/common.h>
 
-#include <chrono>
 #include <iostream>
 
 #include "ConnectGazeboToRosTopic.pb.h"
-#include "ConnectRosToGazeboTopic.pb.h"
-#include "PointStamped.pb.h"
-#include "TransformStamped.pb.h"
-#include "TransformStampedWithFrameIds.pb.h"
 #include "Vector3dStamped.pb.h"
 
 namespace gazebo {
-
-GazeboCablePlugin::~GazeboCablePlugin() {}
-
 void GazeboCablePlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
   model_ = _model;
   world_ = model_->GetWorld();
 
-  if (_sdf->HasElement("robotNamespace"))
+  if (_sdf->HasElement("robotNamespace")) {
     namespace_ = _sdf->GetElement("robotNamespace")->Get<std::string>();
-  else
+  } else {
     gzerr << "[gazebo_cable_plugin] Please specify a robotNamespace.\n";
-
+  }
   node_handle_ = gazebo::transport::NodePtr(new transport::Node());
-
   // Initialise with default namespace (typically /gazebo/default/)
   node_handle_->Init();
-  for (auto link : model_->GetLinks()) {
-    std::cout << link->GetName() << "\n";
+  
+  if (_sdf->HasElement("linkName")) {
+    std::string link_name = _sdf->GetElement("linkName")->Get<std::string>();
+    link_ = model_->GetLink(link_name);
+    if (link_ == NULL) {
+      gzthrow("[gazebo_cable_plugin] Couldn't find specified link \""
+              << link_name << "\".");
+    }
+  } else {
+    gzthrow("[gazebo_cable_plugin] Please specify a linkName.\n");
   }
 
-  if (_sdf->HasElement("linkName"))
-    link_name_ = _sdf->GetElement("linkName")->Get<std::string>();
-  else
-    gzerr << "[gazebo_cable_plugin] Please specify a linkName.\n";
-  link_ = model_->GetLink(link_name_);
-  if (link_ == NULL)
-    gzthrow("[gazebo_cable_plugin] Couldn't find specified link \""
-            << link_name_ << "\".");
+  if (_sdf->HasElement("parentLinkName")) {
+    std::string link_name = _sdf->GetElement("parentLinkName")->Get<std::string>();
+    parent_link_ = model_->GetLink(link_name);
+    if (parent_link_ == NULL) {
+      gzthrow("[gazebo_cable_plugin] Couldn't find specified link \""
+              << link_name << "\".");
+    }
+  } else {
+    gzthrow("[gazebo_cable_plugin] Please specify a parentLinkName.\n");
+  }
 
-  if (_sdf->HasElement("jointName"))
-    joint_name_ = _sdf->GetElement("jointName")->Get<std::string>();
-  else
-    gzerr << "[gazebo_cable_plugin] Please specify a jointName.\n";
-  joint_ = model_->GetJoint(joint_name_);
-  if (joint_ == NULL)
-    gzthrow("[gazebo_cable_plugin] Couldn't find specified joint \""
-            << joint_name_ << "\".");
-
-  getSdfParam<double>(_sdf, "ballRadius", ball_radius_, ball_radius_);
-  getSdfParam<double>(_sdf, "ballWeight", ball_weight_, ball_weight_);
+  getSdfParam<double>(_sdf, "ballMass", ball_mass_, ball_mass_);
   getSdfParam<double>(_sdf, "cableLength", cable_length_, cable_length_);
-  getSdfParam<std::string>(_sdf, "parentFrameId", parent_frame_id_,
-                           parent_frame_id_);
-  getSdfParam<std::string>(_sdf, "childFrameId", child_frame_id_,
-                           child_frame_id_);
   getSdfParam<std::string>(_sdf, "poseTopic", pose_pub_topic_, pose_pub_topic_);
 
-  std::cout << "[CABLE]\n"
-            << parent_frame_id_ << "\n"
-            << child_frame_id_ << "\n";
-
-  parent_link_ = world_->EntityByName(parent_frame_id_);
-  if (parent_link_ == NULL && parent_frame_id_ != kDefaultParentFrameId) {
-    gzthrow("[gazebo_odometry_plugin] Couldn't find specified parent link \""
-            << parent_frame_id_ << "\".");
-  }
+  mav_mass_ = parent_link_->GetInertial()->Mass();
 
   // Listen to the update event. This event is broadcast every
   // simulation iteration.
@@ -90,57 +68,55 @@ void GazeboCablePlugin::OnUpdate(const common::UpdateInfo& _info) {
   // C denotes child frame, P parent frame, and W world frame.
   // Further C_pose_W_P denotes pose of P wrt. W expressed in C.
   ignition::math::Pose3d W_pose_W_C = link_->WorldCoGPose();
-  ignition::math::Vector3d W_unit_pos_W_C = W_pose_W_C.Pos().Normalize();
+  ignition::math::Vector3d W_vel_W_C = link_->WorldLinearVel();
+  ignition::math::Pose3d W_pose_W_P = parent_link_->WorldCoGPose();
+  ignition::math::Vector3d W_vel_W_P = parent_link_->WorldLinearVel();
 
-  ignition::math::Matrix3d W_inertia_matrix_W_C = link_->WorldInertiaMatrix();
-  ignition::math::Vector3d W_angular_momentum_W_C =
-      link_->WorldAngularMomentum();
-  ignition::math::Vector3d W_angular_velocity_W_C =
-      W_inertia_matrix_W_C.Inverse() * W_angular_momentum_W_C;
+  ignition::math::Vector3d W_pos_P_C = (W_pose_W_C - W_pose_W_P).Pos();
+  ignition::math::Vector3d W_unit_pos_P_C(W_pos_P_C);
+  W_unit_pos_P_C.Normalize();
 
-  ignition::math::Vector3d W_vel_W_C =
-      W_angular_velocity_W_C.Cross(W_unit_pos_W_C);
+  ignition::math::Vector3d W_angular_momentum_P_C = -parent_link_->WorldAngularMomentum();
+  ignition::math::Vector3d W_angular_velocity_P_C = parent_link_->WorldInertiaMatrix().Inverse() * W_angular_momentum_P_C;
+  ignition::math::Vector3d W_vel_P_C = W_angular_velocity_P_C.Cross(W_unit_pos_P_C);
 
-  std::cout << "******\n\n" << joint_->GetForce(0) << "\n\n******" << std::endl;
+  if (W_pos_P_C.Length() >= (cable_length_- 1e-3)) {
+    //calculation of thrust
+    ignition::math::Vector3d f = parent_link_->WorldForce() - (ball_mass_ + mav_mass_) * world_->Gravity();
 
-  // thrust get from motorForcePubTopic - expose topic
+    //(m_q + m_l)(v_l_dot + g e3) = (q f R e3 - m_q l (q_dot q_dot)) q
+    ignition::math::Vector3d W_force_W_C = (W_unit_pos_P_C.Dot(f) - ball_mass_ * cable_length_ * (W_vel_P_C.Dot(W_vel_P_C))) * W_unit_pos_P_C;
+    ignition::math::Vector3d W_accel_W_C = (W_force_W_C / (ball_mass_ + mav_mass_)) + world_->Gravity();
+    ignition::math::Vector3d tension = ball_mass_ * (W_accel_W_C - world_->Gravity());
 
-  //   ignition::math::Vector3d C_linear_velocity_W_C =
-  //   link_->RelativeLinearVel(); ignition::math::Vector3d
-  //   C_angular_velocity_W_C = link_->RelativeAngularVel();
+    ignition::math::Vector3d W_fixed_pos_P_C = W_pos_P_C * cable_length_ / W_pos_P_C.Length();
+    ignition::math::Quaterniond W_fixed_rot_P_C = (W_pose_W_C - W_pose_W_P).Rot();
+    ignition::math::Pose3d W_fixed_pose_P_C(W_fixed_pos_P_C, W_fixed_rot_P_C);
+    ignition::math::Vector3d W_fixed_vel_W_C = W_vel_W_C - (W_vel_W_C - W_vel_W_P).Dot(W_unit_pos_P_C) * W_unit_pos_P_C;
 
-  //   ignition::math::Vector3d gazebo_linear_velocity = C_linear_velocity_W_C;
-  //   ignition::math::Vector3d gazebo_angular_velocity =
-  //   C_angular_velocity_W_C; ignition::math::Pose3d gazebo_pose = W_pose_W_C;
+    bool is_paused = world_->IsPaused();
+    if (!is_paused) {
+      world_->SetPaused(true);
+    }
+    link_->SetRelativePose(W_fixed_pose_P_C);
 
-  //   //   ignition::math::Vector3d thrust;
+    // set link_ velocity to desired twist
+    link_->SetLinearVel(W_fixed_vel_W_C);
+    // link_->SetAngularVel(ignition::math::Vector3d(0,0,0));
 
-  //   if (parent_frame_id_ != kDefaultParentFrameId) {
-  //     ignition::math::Pose3d W_pose_W_P = parent_link_->WorldPose();
-  //     ignition::math::Vector3d P_linear_velocity_W_P =
-  //         parent_link_->RelativeLinearVel();
-  //     ignition::math::Vector3d P_angular_velocitfy_W_P =
-  //         parent_link_->RelativeAngularVel();
-  //     // thrust = parent_link_->RelativeLinearAccel();
+    link_->AddForce(tension);
+    parent_link_->AddForce(-tension);
+    world_->SetPaused(is_paused);
+  }
 
-  //     ignition::math::Pose3d P_pose_P_C = W_pose_W_C - W_pose_W_P;
-  //     ignition::math::Vector3d P_linear_velocity_P_C;
-
-  //     P_linear_velocity_P_C = -P_angular_velocity_W_P.Cross(P_pose_P_C.Pos())
-  //     +
-  //                             P_pose_P_C.Rot() * C_linear_velocity_W_C -
-  //                             P_linear_velocity_W_P;
-
-  //     // C_angular_velocity_W_C = 0; // assume child does not rotate on its
-  //     own gazebo_angular_velocity =
-  //         P_pose_P_C.Rot() * C_angular_velocity_W_C - P_angular_velocity_W_P;
-  //     gazebo_linear_velocity = P_linear_velocity_P_C;
-  //     gazebo_pose = P_pose_P_C;
-  //   }
-
-  //   ignition::math::Vector3d tension(0.0, 0.0, 0.0);
-  //   if (gazebo_pose.norm() >= cable_length_) {
-  //   }
+  gz_geometry_msgs::Vector3dStamped pose_msg;
+  pose_msg.mutable_header()->set_frame_id("world");
+  pose_msg.mutable_header()->mutable_stamp()->set_sec((world_->SimTime()).sec);
+  pose_msg.mutable_header()->mutable_stamp()->set_nsec((world_->SimTime()).nsec);
+  pose_msg.mutable_position()->set_x(W_pose_W_C.X());
+  pose_msg.mutable_position()->set_y(W_pose_W_C.Y());
+  pose_msg.mutable_position()->set_z(W_pose_W_C.Z());
+  pose_pub_->Publish(pose_msg);
 }
 
 void GazeboCablePlugin::CreatePubsAndSubs() {
@@ -161,12 +137,49 @@ void GazeboCablePlugin::CreatePubsAndSubs() {
       gz_std_msgs::ConnectGazeboToRosTopic::VECTOR_3D_STAMPED);
   connect_gazebo_to_ros_topic_pub->Publish(connect_gazebo_to_ros_topic_msg,
                                            true);
-
-  broadcast_transform_pub_ =
-      node_handle_->Advertise<gz_geometry_msgs::TransformStampedWithFrameIds>(
-          "~/" + kBroadcastTransformSubtopic, 1);
 }
 
 GZ_REGISTER_MODEL_PLUGIN(GazeboCablePlugin);
-
 }  // namespace gazebo
+
+// std::cout << "******\n\n"
+//           << tension << "\n*" << W_pos_P_C.Length() << "\n*" << world_->Gravity() << "\n\n******" << std::endl;
+// for (auto link : model_->GetLinks())
+// {
+//   std::cout << link->GetName() << "\n";
+// }
+
+// thrust get from motorForcePubTopic - expose topic
+
+//   ignition::math::Vector3d C_linear_velocity_W_C =
+//   link_->RelativeLinearVel(); ignition::math::Vector3d
+//   C_angular_velocity_W_C = link_->RelativeAngularVel();
+
+//   ignition::math::Vector3d gazebo_linear_velocity = C_linear_velocity_W_C;
+//   ignition::math::Vector3d gazebo_angular_velocity =
+//   C_angular_velocity_W_C; ignition::math::Pose3d gazebo_pose = W_pose_W_C;
+
+//   //   ignition::math::Vector3d thrust;
+
+//   if (parent_frame_id_ != kDefaultParentFrameId) {
+//     ignition::math::Pose3d W_pose_W_P = parent_link_->WorldPose();
+//     ignition::math::Vector3d P_linear_velocity_W_P =
+//         parent_link_->RelativeLinearVel();
+//     ignition::math::Vector3d P_angular_velocitfy_W_P =
+//         parent_link_->RelativeAngularVel();
+//     // thrust = parent_link_->RelativeLinearAccel();
+
+//     ignition::math::Pose3d P_pose_P_C = W_pose_W_C - W_pose_W_P;
+//     ignition::math::Vector3d P_linear_velocity_P_C;
+
+//     P_linear_velocity_P_C = -P_angular_velocity_W_P.Cross(P_pose_P_C.Pos())
+//     +
+//                             P_pose_P_C.Rot() * C_linear_velocity_W_C -
+//                             P_linear_velocity_W_P;
+
+//     // C_angular_velocity_W_C = 0; // assume child does not rotate on its
+//     own gazebo_angular_velocity =
+//         P_pose_P_C.Rot() * C_angular_velocity_W_C - P_angular_velocity_W_P;
+//     gazebo_linear_velocity = P_linear_velocity_P_C;
+//     gazebo_pose = P_pose_P_C;
+//   }
